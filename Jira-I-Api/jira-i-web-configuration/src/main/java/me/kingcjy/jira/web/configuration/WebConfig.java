6187ef9b56a41c163f573281f1a6cf7228671bdc
KingCjy <tlssycks@gmail.com>
2019-10-24 13:14:02 +0900

    README.md 파일 변경

--- Jira-I-Api/jira-i-web-configuration/src/main/java/me/kingcjy/jira/web/configuration/WebConfig.java
+++ Jira-I-Api/jira-i-web-configuration/src/main/java/me/kingcjy/jira/web/configuration/WebConfig.java
@@ -1,6 +1,5 @@
 package me.kingcjy.jira.web.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import me.kingcjy.jira.web.filter.HttpLoggingFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
